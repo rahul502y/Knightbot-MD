a546f7b44ad8487f44a3d87f3550a7783dff5e03
rahul502y <rahulsinduriya6@gmail.com>
2025-10-17 06:01:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ const soraCommand = require('./commands/sora');
 global.packname = settings.packname;
 global.author = settings.author;
 global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.ytch = "Rahul sinduriya";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -130,7 +130,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'Rahul-Sinduriya_Y bot',
             serverMessageId: -1
         }
     }
